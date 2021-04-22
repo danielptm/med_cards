b54a6799388f62517d7d51bcdf07675992ef608b
Daniel Tuttle <danieltuttle@Daniels-MacBook-Pro.local>
2021-04-21 19:41:47 -0700

    Got the query to work with name

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -22,57 +22,114 @@ export class SearchComponent implements OnInit {
   }
 
   search(text): void {
-    this.fhir.getPatient(text)
-      .then((response) => {
-        // 9da7d8c2-daef-4722-832e-dcf495d13a4e
-        // 8d1f86a8-3284-43bd-b45e-5931948c97e4
-        if (! response.entry) {
-          alert(`Object with id ${text} does not exist`);
-        }
-        console.log(response);
-        const patientState = new Patient();
-        patientState.id = response.entry[0].resource.id;
-        patientState.firstName = response.entry[0].resource.name[0].given[0];
-        patientState.lastName = response.entry[0].resource.name[0].family;
-        patientState.gender = response.entry[0].resource.gender;
-        patientState.birthDate = response.entry[0].resource.birthDate;
+    if (text.includes('0') || text.includes('1') || text.includes('2') || text.includes('3') || text.includes('4') || text.includes('5') || text.includes('6') || text.includes('7') || text.includes('8') || text.includes('9') || text.includes('-')) {
+      this.fhir.getPatientById(text)
+        .then((response) => {
+          // 9da7d8c2-daef-4722-832e-dcf495d13a4e
+          // 8d1f86a8-3284-43bd-b45e-5931948c97e4
+          if (! response.entry) {
+            alert(`Object with id ${text} does not exist`);
+          }
+          console.log(response);
+          const patientState = new Patient();
+          patientState.id = response.entry[0].resource.id;
+          patientState.firstName = response.entry[0].resource.name[0].given[0];
+          patientState.lastName = response.entry[0].resource.name[0].family;
+          patientState.gender = response.entry[0].resource.gender;
+          patientState.birthDate = response.entry[0].resource.birthDate;
 
-        this.fhir.getConditionsForPatient(text)
-          .then(result => {
-            const x = result;
-            patientState.conditions.push(...this.parser.getConditions(result));
-            this.patientService.setPatient(patientState);
-          });
 
-        this.fhir.getObservationsForPatient(text)
-          .then(result => {
-            patientState.observations.push(...this.parser.getObservations(result));
-            this.patientService.setPatient(patientState);
-          });
+          this.fhir.getConditionsForPatient(patientState.id)
+            .then(result => {
+              const x = result;
+              patientState.conditions.push(...this.parser.getConditions(result));
+              this.patientService.setPatient(patientState);
+            });
 
-        this.fhir.getClinicalImpressionForPatient(text)
-          .then(result => {
-            patientState.clinicalImpressions.push(...this.parser.getClinicalImpressions(result));
-            this.patientService.setPatient(patientState);
-          });
+          this.fhir.getObservationsForPatient(patientState.id)
+            .then(result => {
+              patientState.observations.push(...this.parser.getObservations(result));
+              this.patientService.setPatient(patientState);
+            });
 
-        this.fhir.getDiagnosticReportForpatient(text)
-          .then(result => {
-            patientState.diagnosticImages.push(...this.parser.getImagingStudies(result));
-            this.patientService.setPatient(patientState);
-          });
+          this.fhir.getClinicalImpressionForPatient(patientState.id)
+            .then(result => {
+              patientState.clinicalImpressions.push(...this.parser.getClinicalImpressions(result));
+              this.patientService.setPatient(patientState);
+            });
 
-        this.fhir.getMedicationRequestsForPatient(text)
-          .then(result => {
-            patientState.medications.push(...this.parser.getMedicationRequests(result));
-            this.patientService.setPatient(patientState);
-          });
-        setTimeout(() => {
-          this.router.navigate(['/profile']);
-        }, 1500);
-      }, e => {
-        alert('There was an error with id: ' + text);
-      });
+          this.fhir.getDiagnosticReportForpatient(patientState.id)
+            .then(result => {
+              patientState.diagnosticImages.push(...this.parser.getImagingStudies(result));
+              this.patientService.setPatient(patientState);
+            });
+
+          this.fhir.getMedicationRequestsForPatient(patientState.id)
+            .then(result => {
+              patientState.medications.push(...this.parser.getMedicationRequests(result));
+              this.patientService.setPatient(patientState);
+            });
+          setTimeout(() => {
+            this.router.navigate(['/profile']);
+          }, 1500);
+        }, e => {
+          alert('There was an error with id: ' + text);
+        });
+    } else {
+      this.fhir.getPatientByName(text)
+        .then((response) => {
+          // 9da7d8c2-daef-4722-832e-dcf495d13a4e
+          // 8d1f86a8-3284-43bd-b45e-5931948c97e4
+          if (! response.entry) {
+            alert(`Object with id ${text} does not exist`);
+          }
+          console.log(response);
+          const patientState = new Patient();
+          patientState.id = response.entry[0].resource.id;
+          patientState.firstName = response.entry[0].resource.name[0].given[0];
+          patientState.lastName = response.entry[0].resource.name[0].family;
+          patientState.gender = response.entry[0].resource.gender;
+          patientState.birthDate = response.entry[0].resource.birthDate;
+
+          this.fhir.getConditionsForPatient(patientState.id)
+            .then(result => {
+              const x = result;
+              patientState.conditions.push(...this.parser.getConditions(result));
+              this.patientService.setPatient(patientState);
+            });
+
+          this.fhir.getObservationsForPatient(patientState.id)
+            .then(result => {
+              patientState.observations.push(...this.parser.getObservations(result));
+              this.patientService.setPatient(patientState);
+            });
+
+          this.fhir.getClinicalImpressionForPatient(patientState.id)
+            .then(result => {
+              patientState.clinicalImpressions.push(...this.parser.getClinicalImpressions(result));
+              this.patientService.setPatient(patientState);
+            });
+
+          this.fhir.getDiagnosticReportForpatient(patientState.id)
+            .then(result => {
+              patientState.diagnosticImages.push(...this.parser.getImagingStudies(result));
+              this.patientService.setPatient(patientState);
+            });
+
+          this.fhir.getMedicationRequestsForPatient(patientState.id)
+            .then(result => {
+              patientState.medications.push(...this.parser.getMedicationRequests(result));
+              this.patientService.setPatient(patientState);
+            });
+          setTimeout(() => {
+            this.router.navigate(['/profile']);
+          }, 1500);
+        }, e => {
+          alert('There was an error with id: ' + text);
+        });
+    }
+
+
   }
 
 }
